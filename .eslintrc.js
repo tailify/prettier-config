d0a637e8d597f81f134d1fbf2ab0a3621ab958a2
Jamie Connolly <jamie.connolly@tailify.com>
2019-01-09 11:59:46 +0000

    Use @tailify/eslint-config-legacy for ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,6 @@
 'use strict';
 
-const { configs: jestConfig } = require('eslint-plugin-jest');
-
 module.exports = {
+  extends: ['@tailify/legacy'],
   root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:node/recommended',
-    'plugin:prettier/recommended',
-  ],
-  parserOptions: {
-    sourceType: 'module',
-  },
-  plugins: ['node'],
-  rules: {
-    'linebreak-style': 'error',
-    'quotes': ['error', 'single', { avoidEscape: true }],
-    'sort-keys': 'error',
-  },
-  overrides: [
-    {
-      files: ['**/*.test.js'],
-      ...jestConfig.recommended,
-      env: {
-        jest: true,
-      },
-    },
-  ],
 };
