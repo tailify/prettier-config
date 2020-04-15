2442ded7cd6aecf66310396ddb9fe265460d3d43
Vladimir Aleksiev <vladimir.aleksiev@tailify.com>
2020-04-15 12:45:02 +0300

    Bump prettier to 2.0.1 (#7)
    * bump prettier
    add one more test
    
    * remove () from functions
    
    * drop node8 support
    clean up greenkeeper

--- tests/index.test.js
+++ tests/index.test.js
@@ -7,3 +7,11 @@ it('should not contain invalid options', () => {
   const result = prettier.format('foo ( );\n', Object.assign(options, { filepath: 'foo.js' }));
   expect(result).toBe('foo();\n');
 });
+
+it('lints properly', () => {
+  const result = prettier.format(
+    'const foo = (arg) => {};\n',
+    Object.assign(options, { filepath: 'foo.js' })
+  );
+  expect(result).toBe('const foo = arg => {};\n');
+});
